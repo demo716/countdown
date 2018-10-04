e5ae37c648c6da575c9f4c68706193f0a43ec49a
Marino Demo <demo716@gmail.com>
2018-10-04 15:25:51 -0400

    Created list component

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hello World!</Text>
-        <Text>Goodbye World :(</Text>
-      </View>
+      <EventList />
     );
   }
 }
