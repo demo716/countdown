9830b1d18fd66d1520d8c38804c42f0d852d4327
Marino Demo <demo716@gmail.com>
2018-10-04 18:31:31 -0400

    Updated events with timer

--- EventList.js
+++ EventList.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
+
+import EventCard from './EventCard';
+import { now } from 'moment';
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#F3F3F3'
+  },
+});
 
 class EventList extends Component {
+
+  state = {
+    events: []
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now(),
+        }))
+      });
+    }, 1000);
+
+    const events = require('./db.json').events.map(evt => ({
+      ...evt,
+      date: new Date(evt.date),
+    }));
+    this.setState({ events })
+  }
+
+
   render () {
     return (
       <FlatList
-        data={[{name: 'a'}, {name: 'b'}]}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        style={styles.list}
+        data={this.state.events}
+        renderItem={({ item }) => <EventCard event={item} />}
+        keyExtractor={item => item.id}
       />
     )
   }
