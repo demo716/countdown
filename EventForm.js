81b1bdecf0ecc93e7145d0012d5251f7d43ca559
Marino Demo <demo716@gmail.com>
2018-10-05 12:09:10 -0400

    Added input field in new event

--- EventForm.js
+++ EventForm.js
@@ -3,7 +3,23 @@ import {
   View,
   Text,
   TouchableHighlight,
+  TextInput,
+  StyleSheet
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  fieldContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+  },
+  text: {
+    height: 40,
+    margin: 0,
+    marginRight: 7,
+    paddingLeft: 10,
+  }
+});
 
 class EventForm extends Component {
 
@@ -13,7 +29,18 @@ class EventForm extends Component {
 
   render () {
     return (
-      <View>
+      <View
+        style={{
+          flex: 1
+        }}
+      >
+        <View style={styles.fieldContainer}>
+          <TextInput
+            style={styles.text}
+            placeholder="Event title"
+            spellCheck={false}
+          />
+        </View>
         <TouchableHighlight
           onPress={this.handleAddPress}
         >
